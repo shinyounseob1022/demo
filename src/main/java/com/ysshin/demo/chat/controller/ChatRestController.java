84fbd1cac36e7f56b0ecad33d607f3f6462b136c
shinyounseob1022 <shinyounseob1022@gmail.com>
2024-05-20 18:00:38 +0900

    Feat: "채팅방 생성 기능 구현"

--- src/main/java/com/ysshin/demo/chat/controller/ChatRestController.java
+++ src/main/java/com/ysshin/demo/chat/controller/ChatRestController.java
@@ -1,11 +1,10 @@
 package com.ysshin.demo.chat.controller;
 
+import com.ysshin.demo.chat.dto.ChatRoomDto;
 import com.ysshin.demo.chat.service.ChatService;
 import com.ysshin.demo.common.ResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/chats")
@@ -14,8 +13,14 @@ public class ChatRestController {
 
     private final ChatService chatService;
 
+    @PostMapping("/room")
+    public ResponseDto createChatRoom(@RequestBody ChatRoomDto chatRoomDto) {
+        return chatService.createChatRoom(chatRoomDto);
+    }
+
     @GetMapping("/rooms")
     public ResponseDto getRoomList() {
         return chatService.getRoomList();
     }
+
 }
---
+++ src/main/java/com/ysshin/demo/chat/repository/ChatRoomMemberRepository.java
@@ -0,0 +1,7 @@
+package com.ysshin.demo.chat.repository;
+
+import com.ysshin.demo.chat.entity.ChatRoomMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRoomMemberRepository extends JpaRepository<ChatRoomMember, Long> {
+}
